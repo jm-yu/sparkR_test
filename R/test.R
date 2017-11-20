b2ab3e53e1e3e1616c20f380e3d93259d287cc3e
jmyu1994 <jmyu1994@gmail.com>
2017-11-19 22:42:05 -0500

    update R file

--- R/test.R
+++ R/test.R
@@ -1,15 +1,21 @@
 if (nchar(Sys.getenv("SPARK_HOME")) < 1) {
     Sys.setenv(SPARK_HOME = "/opt/spark-1.6.0-bin-hadoop2.6")
 }
+
+
 library(SparkR, lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))
-sparkR.stop()
 sc <- sparkR.init(master = "local[3]", sparkEnvir = list(spark.driver.memory="2g"),sparkPackages="com.databricks:spark-csv_2.10:1.4.0")
 sqlContext <- sparkRSQL.init(sc)
+
 train_df <- read.df(sqlContext,"./train_df.csv","csv", header = "true", inferSchema = "true")
-head(train_df)
+#head(train_df)
+
 jfk_flights <- filter(train_df, train_df$ORIGIN == "JFK")
-cache(jfk_flights)
+
+#cache(jfk_flights)
 #unpersist(jfk_flights)
+
+
 start_time <- Sys.time()
 head(agg(group_by(jfk_flights, jfk_flights$DEST), count = n(jfk_flights$DEST)), 5L)
 end_time <- Sys.time()
