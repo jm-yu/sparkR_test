0ce0b7a68af78f18c26b7d0e01048d76ab4f4589
jmyu1994 <jmyu1994@gmail.com>
2017-11-20 23:38:42 -0500

    update code

--- R/test.R
+++ R/test.R
@@ -1,18 +1,15 @@
 if (nchar(Sys.getenv("SPARK_HOME")) < 1) {
-    Sys.setenv(SPARK_HOME = "/opt/spark-1.6.0-bin-hadoop2.6")
+  Sys.setenv(SPARK_HOME = "/opt/spark-1.6.0-bin-hadoop2.6")
 }
-
-
 library(SparkR, lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))
-sc <- sparkR.init(master = "local[3]", sparkEnvir = list(spark.driver.memory="2g"),sparkPackages="com.databricks:spark-csv_2.10:1.4.0")
+sparkR.stop()
+sc <- sparkR.init(master = "local[3]",sparkPackages="com.databricks:spark-csv_2.10:1.4.0")
 sqlContext <- sparkRSQL.init(sc)
-
+start_time
 train_df <- read.df(sqlContext,"./train_df.csv","csv", header = "true", inferSchema = "true")
-#head(train_df)
-
+head(train_df)
 jfk_flights <- filter(train_df, train_df$ORIGIN == "JFK")
-
-#cache(jfk_flights)
+cache(jfk_flights)
 #unpersist(jfk_flights)
 
 
@@ -20,3 +17,6 @@ start_time <- Sys.time()
 head(agg(group_by(jfk_flights, jfk_flights$DEST), count = n(jfk_flights$DEST)), 5L)
 end_time <- Sys.time()
 end_time - start_time
+model <- glm(arrDelay ~ depDelay + distance, family = "gaussian", data = training)
+
+
