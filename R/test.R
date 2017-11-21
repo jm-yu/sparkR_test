2c6c0a1ab7a48b22d7ad84bed852ffbe4f951b66
jmyu1994 <jmyu1994@gmail.com>
2017-11-21 16:21:47 -0500

    finished

--- R/test.R
+++ R/test.R
@@ -1,22 +1,39 @@
 if (nchar(Sys.getenv("SPARK_HOME")) < 1) {
-  Sys.setenv(SPARK_HOME = "/opt/spark-1.6.0-bin-hadoop2.6")
+    Sys.setenv(SPARK_HOME = "/opt/spark-1.6.0-bin-hadoop2.6")
 }
 library(SparkR, lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))
-sparkR.stop()
+#sparkR.stop()
 sc <- sparkR.init(master = "local[3]",sparkPackages="com.databricks:spark-csv_2.10:1.4.0")
 sqlContext <- sparkRSQL.init(sc)
-start_time
-train_df <- read.df(sqlContext,"./train_df.csv","csv", header = "true", inferSchema = "true")
-head(train_df)
-jfk_flights <- filter(train_df, train_df$ORIGIN == "JFK")
+
+#load data
+df <- read.df(sqlContext,"./train_df.csv","csv", header = "true", inferSchema = "true")
+
+printSchema(df)
+head(df)
+
+#some operations of sparkR
+head(select(df, df$ARR_DELAY));
+
+head(select(filter(df, df$ARR_DELAY < 7), df$ARR_DELAY))
+
+jfk_flights <- filter(df, df$ORIGIN == "JFK")
+head(jfk_flights)
+
+#to compare running timw with and without cache.
 cache(jfk_flights)
 #unpersist(jfk_flights)
-
-
 start_time <- Sys.time()
-head(agg(group_by(jfk_flights, jfk_flights$DEST), count = n(jfk_flights$DEST)), 5L)
+jfk_dests <- summarize(group_by(jfk_flights, jfk_flights$DEST), count = n(jfk_flights$DEST))
+head(arrange(jfk_dests, desc(jfk_dests$count)))
 end_time <- Sys.time()
 end_time - start_time
-model <- glm(arrDelay ~ depDelay + distance, family = "gaussian", data = training)
 
+
+#use SQL to query the data
+head(df)
+sqlContext <- sparkRSQL.init(sc)
+registerTempTable(df, "flights")
+lax_flights <- sql(sqlContext, "SELECT * from flights where ORIGIN = 'LAX' and DAY_OF_MONTH = 1")
+head(lax_flights)
 
--- README.txt
+++ README.txt
@@ -1,8 +1,8 @@
-Docker + RStudio + sparkR
-set up RStudio server with SparkR. Compare the performance of some operations of sparkR and R.
+This is a demo for presenting sparkR.
 
+Docker + R Studio + sparkR
 
-set up RStudio server with SparkR:
+set up R Studio server with SparkR on docker:
 
 $docker run -d -v /Users/jmyu:/home/rstudio -p 8787:8787 --name sparkrstudio vinicius85/sparkr-rstudio
 
@@ -10,14 +10,14 @@ $docker pull vinicius85/spark-rstudio
 
 $docker exec -it sparkrstudio /bin/bash
 
-adduser username #set RStudio username
+adduser username #set R studio username
 
 after setting server. use ifconfig to get ip.
 $ifconfig | grep inet
 
-open a browser and enter “ip:8787” to open Rstudio.
+open a browser and enter “ip:8787” to open R studio.
 
-test codes will be updated later.
+run codes on R studio
 
 reference: 
 https://github.com/vinicius85/docker/tree/master/sparkr-rstudio
